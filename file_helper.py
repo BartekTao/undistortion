4ef8fec9d1d4089ec7f01a116d39d2fa9e4aa0b5
bartektao <bartek.tao@framy.co>
2022-10-07 18:01:09 +0800

    add decode img function

--- file_helper.py
+++ file_helper.py
@@ -1,5 +1,14 @@
 import main
+import cv2
+import base64
+import pickle
 
 def saveConfig():
     with open('config.ini', 'w') as configfile:
         main.config.write(configfile)
+
+def coverToCV2(data):
+    imdata = base64.b64decode(data)
+    buf = pickle.loads(imdata)
+    image = cv2.imdecode(buf, cv2.IMREAD_COLOR)
+    return image
